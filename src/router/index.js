08c9371643f053463a3fb3b40c6f5e06677eb93d
Believe-H <885774045@qq.com>
2019-07-09 20:41:54 +0800

    login模块基础布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 // 创建路由对象,配置路由规则和其他路由配置
 import VueRouter from 'vue-router'
 import Vue from 'vue'
+import Login from '@/views/login'
 Vue.use(VueRouter)
 const router = new VueRouter({
-  routes: []
+  routes: [
+    { path: '/login', name: 'login', component: Login }
+  ]
 })
 export default router
---
+++ src/views/login/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <!-- 卡片 -->
+    <el-card class="login_box">
+      <img src="../../assets/images/logo_index.png" alt />
+
+    </el-card>
+
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang='less'>
+.container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: url(../../assets/images/login_bg.jpg) no-repeat center/cover;
+  .login_box {
+    width: 400px;
+    height: 300px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      display: block;
+      width: 200px;
+      margin: 5px auto 20px;
+    }
+  }
+}
+</style>
