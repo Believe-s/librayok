d28a7a39543e00a52c54bd9cfd11e92d64a90744
Believe-H <885774045@qq.com>
2019-07-09 16:41:40 +0800

    目录创建完成

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 职责1: 依赖项目需要的资源(js模块 css资源 ...)
+// 职责2: 实例化根实例 render() vue实例中的一个选项 渲染App组件,然后挂载到#app的容器内
 new Vue({
   render: h => h(App)
 }).$mount('#app')
